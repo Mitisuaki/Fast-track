46fec875c5037812789934bdac99193b4c69d14e
Miti <mitisuaki@gmail.com>
2023-12-03 19:02:38 -0300

    tasksPage design done

--- src/Controllers/cTaksPage.js
+++ src/Controllers/cTaksPage.js
@@ -1,6 +1,8 @@
 const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
 const userName = document.querySelector(".username");
 
+const taskTableBody = document.querySelector("#task-table-body");
+
 const exit = document.querySelector(".btn-sair");
 
 
@@ -17,4 +19,21 @@ if (currentUser === null) {
 
 }
 
+console.log(taskTableBody.children.length)
 
+// Teste com setInterval
+var intervalID = window.setInterval(checkWeatherAPI, 1000);
+
+function checkWeatherAPI() {
+
+  const nowTime = new Date(2023, 12, 5);
+  const check1 = new Date(2022, 12, 1);
+  const check2 = new Date(2023, 1, 10);
+
+  if (nowTime > check1 && nowTime < check2) {
+
+    console.log("True");
+  } else {
+    console.log("False");
+  }
+}
